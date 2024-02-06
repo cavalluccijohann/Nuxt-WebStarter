3129a443a253c640ddf690a2ce31d68533b2a836
johann <johann.cavallucci@epitech.eu>
2024-02-06 16:07:21 +0100

    FEAT - Test vercel

--- components/Modal.vue
+++ components/Modal.vue
@@ -1,18 +1,24 @@
 <script setup lang="ts">
+const target = ref(null);
+const open = defineModel({type: Boolean});
 
-defineProps({
-  modelValue: Boolean,
+useClickElement(target, () => {
+  open.value = false;
 });
+
+const {x, y} = useMouse();
 
 </script>
 
 <template>
   <Transition name="slide-fade">
     <div
-        v-if="modelValue"
-        class="fixed left-0 w-full height-menu bg-main/20 z-10 backdrop-blur-md flex items-center justify-center gap-6 sm:gap-10 p-2"
+        v-if="open"
+        class="fixed left-0 w-full height-menu bg-main/20 z-10 backdrop-blur-md flex items-center justify-center gap-6 sm:gap-10 p-2 modal-extern"
     >
-      <div class="flex flex-col items-center justify-center w-full md:w-2/3 lg:w-1/3 rounded-2xl p-2 bg-main">
+      <p class="text-main">{{x}}</p>
+      <p class="text-main">{{y}}</p>
+      <div ref="target" class="flex flex-col items-center justify-center w-full md:w-2/3 lg:w-1/3 rounded-2xl p-2 bg-main modal-intern">
         <slot name="header"></slot>
         <div class="w-11/12 h-[1px] bg-secondary rounded-2xl"></div>
         <slot name="body"></slot>
